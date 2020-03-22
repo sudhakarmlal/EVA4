645109646a181671a8307b44169d39726f6aad70
sudhakarmlal <43887344+sudhakarmlal@users.noreply.github.com>
2020-03-22 06:14:08 +0530

    Add files via upload

---
+++ Session9/utils/plot_metrics_utils.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+
+def plot_loss_accuracy(train_losses, train_acc, test_losses, test_acc):
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(train_losses)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_losses)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
